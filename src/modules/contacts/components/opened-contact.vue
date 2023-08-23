3fcde2861a05bb4a122de26c55cf30109bcfc746
liza-pohranichna <haemoglobin0@gmail.com>
2023-08-23 19:30:48 +0300

    feature: delete contact item in contact page in progress [WTEL-3617]

--- src/modules/contacts/components/opened-contact.vue
+++ src/modules/contacts/components/opened-contact.vue
@@ -19,6 +19,12 @@
         @saved="loadItem"
         @close="isContactPopup = false"
       ></contact-popup>
+      <delete-confirmation-popup
+        v-show="isDeleteConfirmationPopup"
+        :delete-count="deleteCount"
+        :callback="deleteCallback"
+        @close="closeDelete"
+      ></delete-confirmation-popup>
       <div class="opened-contact-content">
         <opened-contact-general
           :common-name="itemInstance.name ? itemInstance.name.commonName : ''"
@@ -27,6 +33,10 @@
           :about="itemInstance.about"
           :labels="itemInstance.labels ? itemInstance.labels : []"
           @edit="isContactPopup = true"
+          @delete="askDeleteConfirmation({
+              deleted: [itemInstance.value],
+              callback: () => deleteItem(itemInstance.value),
+            })"
         ></opened-contact-general>
         <opened-contact-tabs
           :namespace="namespace"
@@ -37,10 +47,16 @@
 </template>
 
 <script setup>
-import { useCardStore } from '@webitel/ui-sdk/src/modules/CardStoreModule/composables/useCardStore';
+// import { useCardStore } from '@webitel/ui-sdk/src/modules/CardStoreModule/composables/useCardStore';
+import DeleteConfirmationPopup
+  from '@webitel/ui-sdk/src/modules/DeleteConfirmationPopup/components/delete-confirmation-popup.vue';
+import {
+  useDeleteConfirmationPopup
+} from '@webitel/ui-sdk/src/modules/DeleteConfirmationPopup/composables/useDeleteConfirmationPopup';
 import { onMounted, onUnmounted, computed, ref, provide, reactive, readonly } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute, useRouter } from 'vue-router';
+import { useCardStore } from '../store/useCardStore';
 import ContactPopup from './contact-popup.vue';
 import OpenedContactGeneral from './opened-contact-general.vue';
 import OpenedContactTabs from './opened-contact-tabs.vue';
@@ -59,7 +75,17 @@ const {
   loadItem,
   setId,
   resetState,
+  deleteItem,
 } = useCardStore(baseNamespace);
+
+const {
+  isVisible: isDeleteConfirmationPopup,
+  deleteCount,
+  deleteCallback,
+
+  askDeleteConfirmation,
+  closeDelete,
+} = useDeleteConfirmationPopup();
 
 provide('access', computed(() => ({
   hasRbacEditAccess: itemInstance.value?.access?.edit,
