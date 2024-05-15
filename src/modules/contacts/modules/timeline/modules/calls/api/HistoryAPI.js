183a9c3fd7314bc2467449688990b470b45f6668
dlohvinov <nsblnr@gmail.com>
2024-05-15 21:07:31 +0300

    fix: filtered transferred calls in timeline [WTEL-4534]

--- src/modules/contacts/modules/timeline/modules/calls/api/HistoryAPI.js
+++ src/modules/contacts/modules/timeline/modules/calls/api/HistoryAPI.js
@@ -1,82 +1,16 @@
+import { getDefaultGetListResponse } from '@webitel/ui-sdk/src/api/defaults';
 import {
-  camelToSnake, notify,
-  starToSearch, snakeToCamel,
-  merge, mergeEach,
+  merge,
+  notify,
+  snakeToCamel,
 } from '@webitel/ui-sdk/src/api/transformers';
 import applyTransform
   from '@webitel/ui-sdk/src/api/transformers/applyTransform';
-import { getDefaultGetListResponse } from '@webitel/ui-sdk/src/api/defaults';
-import convertDuration from '@webitel/ui-sdk/src/scripts/convertDuration';
 import { CallServiceApiFactory } from 'webitel-sdk';
-import * as converters from '@webitel/ui-sdk/src/scripts/caseConverters';
-import configuration from '../../../../../../../app/api/openAPIConfig.js';
 import instance from '../../../../../../../app/api/instance.js';
+import configuration from '../../../../../../../app/api/openAPIConfig.js';
 
 const callService = new CallServiceApiFactory(configuration, '', instance);
-
-const computeDate = (timestamp) => {
-  if (!timestamp) return null;
-  const date = new Date(+timestamp);
-  return date.toLocaleDateString();
-};
-
-const computeTime = (timestamp) => {
-  if (!timestamp) return null;
-  const date = new Date(+timestamp);
-  return date.toLocaleTimeString();
-};
-
-const computeDateAndTime = (timestamp) => {
-  if (!timestamp) return null;
-  const date = new Date(+timestamp);
-  return date.toLocaleString('en-GB');
-};
-
-const mapDefaultComments = (item) => {
-  const defaultComment = {
-    startSec: 0,
-    endSec: 0,
-    note: '',
-  };
-
-  return item.annotations ? item.annotations.map((comment) => ({
-    ...defaultComment,
-    ...comment,
-  })) : [];
-};
-
-const transformResponseItems = (items) => {
-  const defaultObject = {
-    _isSelected: false,
-  };
-  return items.map((item) => ({
-    ...defaultObject,
-    ...item,
-    date: computeDate(item.createdAt),
-    time: computeTime(item.createdAt),
-    bridgedAt: computeTime(item.bridgedAt),
-    queueBridgedAt: computeTime(item.queueBridgedAt),
-    createdAt: computeDateAndTime(item.createdAt),
-    answeredAt: computeTime(item.answeredAt),
-    joinedAt: computeTime(item.joinedAt),
-    leavingAt: computeTime(item.leavingAt),
-    hangupAt: computeTime(item.hangupAt),
-    reportingAt: computeTime(item.reportingAt),
-    duration: convertDuration(item.duration),
-    holdSec: convertDuration(item.holdSec),
-    waitSec: convertDuration(item.waitSec),
-    billSec: convertDuration(item.billSec),
-    talkSec: convertDuration(item.talkSec),
-    reportingSec: convertDuration(item.reportingSec),
-    queueWaitSec: convertDuration(item.queueWaitSec),
-    queueDurationSec: convertDuration(item.queueDurationSec),
-    annotations: mapDefaultComments(item),
-    hangupDisposition: item.hangupDisposition
-      ? converters.snakeToCamel(item.hangupDisposition)
-      : '',
-    score: item.scoreRequired ? item.scoreRequired.toFixed(2) : null,
-  }));
-};
 
 const getList = ({
                    paramsTransformers = [],
@@ -100,7 +34,7 @@ const getList = ({
     skipParent,
     parentId,
     cause,
-    fields  = ['to','transfer_to','created_at','duration'],
+    fields = ['to', 'transfer_to', 'created_at', 'duration'],
     sort = '-created_at',
     direction,
     search,
@@ -153,7 +87,9 @@ const getList = ({
       skip_parent: skipParent,
       parent_id: parentId,
       cause,
-      has_file: hasFile === 'true' ? true : hasFile === 'false' ? false : undefined,
+      has_file: hasFile === 'true' ? true : hasFile === 'false'
+        ? false
+        : undefined,
       number: search,
       direction,
       id,
@@ -162,7 +98,8 @@ const getList = ({
       amd_result: amdResult,
       fts,
       directions: hangupDisposition,
-      has_transcript: hasTranscription === 'true' ? true : hasTranscription === 'false' ? false : undefined,
+      has_transcript: hasTranscription === 'true' ? true : hasTranscription ===
+      'false' ? false : undefined,
       agent_description: description,
       grantee_id: grantee,
       talk: talkSec,
@@ -194,7 +131,10 @@ const getHistory = getList({
     merge(getDefaultGetListResponse()),
   ],
   responseItemsTransformers: [
-    // transformResponseItems,
+    (items) => {
+      // filter calls, received transfer - we need only calls that ended by transfer
+      return items.filter(({ transferTo }) => !!transferTo);
+    },
   ],
 });
 
