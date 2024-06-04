fb274376a6ca39f9860aca93b045587210c5c7b7
Lera24 <bedulinavalera21@gmail.com>
2024-06-04 16:47:42 +0300

    fix: display attach[WTEL-4600]

--- src/modules/contacts/modules/timeline/modules/chats/components/point-row/chat-point-timeline-row-file.vue
+++ src/modules/contacts/modules/timeline/modules/chats/components/point-row/chat-point-timeline-row-file.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="chat-point-timeline-row-file">
-    <wt-icon
-      icon="attach"
-    />
-    <span class="chat-point-timeline-row-file__name">
-      {{ filename }}
-    </span>
-  </div>
+      <wt-icon
+        :icon="icon"
+      />
+      <span
+        @click="download"
+        class="chat-point-timeline-row-file__name"
+      >{{ props.file.name }}</span>
+    </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const props = defineProps({
   file: {
@@ -18,18 +22,36 @@ const props = defineProps({
   },
 });
 
-const filename = computed(() => {
-  return `${props.file.name}.${props.file.type}`;
-});
+const icon = computed(() => {
+  if (props.file.type.includes('image')) return 'preview-tag-image';
+  if (props.file.type.includes('video')) return 'preview-tag-video';
+  if (props.file.type.includes('audio')) return 'preview-tag-audio';
+  return 'attach';
+})
+
+function download() {
+  const a = document.createElement('a');
+  const token = localStorage.getItem('access-token');
+  a.href = `${API_URL}/storage/file/${props.file.id}/download?access_token=${token}`;
+  a.target = '_blank';
+  a.download = props.file.name;
+  a.click();
+}
 </script>
 
 <style scoped lang="scss">
 .chat-point-timeline-row-file {
   display: flex;
   align-items: center;
-  gap: var(--spacing-3xs);
+  gap: var(--spacing-xs);
 
   &__name {
+    color: var(--info-color);
+    cursor: pointer;
+
+    &:hover {
+      color: var(--info-hover-color);
+    }
   }
 }
 </style>
