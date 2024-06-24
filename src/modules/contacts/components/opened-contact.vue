f38798b310c24a8e4051ad0eae9a8453e92b475a
Lera24 <bedulinavalera21@gmail.com>
2024-06-24 13:45:26 +0300

    fix: closing new browser tab[WTEL-4575]

--- src/modules/contacts/components/opened-contact.vue
+++ src/modules/contacts/components/opened-contact.vue
@@ -132,7 +132,7 @@ function close() {
   // Need to close the tab if you moved from another application
   // https://webitel.atlassian.net/browse/WTEL-4552
 
-  if(window.history.length === 1) window.close();
+  if(window.history.length === 1) return window.close();
   return router.push('/contacts');
 }
 
