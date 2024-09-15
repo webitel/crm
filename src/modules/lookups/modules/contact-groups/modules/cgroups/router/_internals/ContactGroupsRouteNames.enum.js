340dcbb0b3f47bd98b5c9b9d736bfc12bcf1e60f
Sviatoslav Bar <baterfild2011@gmail.com>
2024-09-15 21:36:45 +0300

    Feat/ contact-groups - api, router store [WTEL-4740]

---
+++ src/modules/lookups/modules/contact-groups/modules/cgroups/router/_internals/ContactGroupsRouteNames.enum.js
@@ -0,0 +1,6 @@
+import RouteNames from "../../../../../../../../app/router/_internals/RouteNames.enum.js";
+
+export default Object.freeze({
+  GENERAL: `${RouteNames.CONTACT_GROUPS}-general`,
+  PERMISSIONS: `${RouteNames.CONTACT_GROUPS}-permissions`,
+});
