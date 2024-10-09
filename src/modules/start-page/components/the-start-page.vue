9af7dd9c3b1282f3b01c60525be6dd51ace87781
Sviatoslav Bar <baterfild2011@gmail.com>
2024-10-09 12:59:26 +0300

    Feat/ move module to ui-sdk [WTEL-4754]

--- src/modules/start-page/components/the-start-page.vue
+++ src/modules/start-page/components/the-start-page.vue
@@ -1,11 +1,6 @@
 <template>
-  <start-page-logo :image="logo"/>
-  <div class="start-page-wrapper">
-    <start-page-card
-      v-for="(card) of navCards"
-      :key="card.value"
-      :card="card"
-    />
+  <div>
+    <start-page :nav="nav"/>
   </div>
 </template>
 
@@ -14,26 +9,9 @@
   import { useI18n } from 'vue-i18n';
   import { useStore } from 'vuex';
   import CrmSections from '@webitel/ui-sdk/src/enums/WebitelApplications/CrmSections.enum';
-  import StartPageCard from './start-page-card.vue';
-  import StartPageLogo from './start-page-logo.vue';
-  import ContactsSecLight from '../assets/contacts-section-light.svg';
-  import ContactsSecDark from '../assets/contacts-section-dark.svg';
-  import ConfigurationSecLight from '../assets/configuration-section-light.svg';
-  import ConfigurationSecDark from '../assets/configuration-section-dark.svg';
-  import StartLogoLight from '../assets/start-page-logo-light.svg';
-  import StartLogoDark from '../assets/start-page-logo-dark.svg';
-
-  // Vuex store access
-  const store = useStore();
+  import StartPage from '@webitel/ui-sdk/src/modules/StartPage/components/the-start-page.vue';
 
   const { t } = useI18n();
-
-  const theme = computed(() => store.state.appearance.theme);
-  const darkMode = computed(() => store.getters['appearance/DARK_MODE']);
-
-  const logo = computed(() => {
-    return darkMode.value ? StartLogoDark : StartLogoLight;
-  });
 
   const nav = [
     {
@@ -48,37 +26,7 @@
       name: 'configuration',
     },
   ]
-
-  const cardSectionPic = {
-    [CrmSections.CONTACTS]: {
-      dark: ContactsSecDark,
-      light: ContactsSecLight,
-    },
-    [CrmSections.CONFIGURATION]: {
-      dark: ConfigurationSecDark,
-      light: ConfigurationSecLight,
-    },
-  };
-
-  const navCards = computed(() => {
-    return nav.map((navItem) => ({
-      ...navItem,
-      disabled: false, // Статус доступу до навігаційних карток
-      name: t(`startPage.${navItem.value}.name`),
-      text: t(`startPage.${navItem.value}.text`),
-      image: darkMode.value ? cardSectionPic[navItem.value].dark : cardSectionPic[navItem.value].light,
-    }));
-  });
 </script>
 
 <style scoped>
-.start-page-wrapper {
-  box-sizing: border-box;
-  min-width: 264px;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 264px);
-  justify-content: center;
-  grid-gap: var(--spacing-sm);
-  padding: var(--spacing-sm);
-}
 </style>
