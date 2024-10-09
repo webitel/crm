9249fabd942e2365e9ee61703ad6b6acd39f0518
Sviatoslav Bar <baterfild2011@gmail.com>
2024-10-10 01:08:55 +0300

    Feat/ guards [WTEL-4754]

---
+++ src/app/router/_internals/RouteNames.enum.js
@@ -0,0 +1,23 @@
+export default Object.freeze({
+  AUTH: 'auth',
+  APPLICATION_HUB: 'application-hub',
+  HOME: 'home',
+  START: 'start',
+
+
+  // LOOKUPS
+  SKILLS: 'skills',
+  BUCKETS: 'buckets',
+  BLACKLIST: 'blacklists',
+  REGIONS: 'regions',
+  CALENDARS: 'calendars',
+  COMMUNICATIONS: 'communications',
+  PAUSE_CAUSE: 'agent-pause-cause',
+  MEDIA: 'media',
+  CONTACT_GROUPS: 'contact-groups',
+
+
+  SETTINGS_PAGE: 'settings',
+  PAGE_403: 'access-denied',
+  PAGE_404: '',
+});
