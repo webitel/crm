b0897c795f0dbea7b23a7966951f95f6b45256a2
Sviatoslav Bar <baterfild2011@gmail.com>
2024-10-10 13:56:14 +0300

    Feat/ temporary store [WTEL-4754]

--- src/app/store/BaseStoreModules/StoreModules/ObjectStoreModule.js
+++ src/app/store/BaseStoreModules/StoreModules/ObjectStoreModule.js
@@ -1,16 +1,31 @@
 import BaseStoreModule from '@webitel/ui-sdk/src/store/BaseStoreModules/BaseStoreModule';
 import deepCopy from 'deep-copy';
 
+const baseState = {
+  headers: [],
+  dataList: [],
+  aggs: {},
+  size: 10,
+  search: '',
+  page: 1,
+  sort: '',
+  isNextPage: false,
+};
+
 export default class ObjectStoreModule extends BaseStoreModule {
-  constructor({ resettableState, headers } = {}) {
+  constructor({ resettableState = {}, headers = [] } = {}) {
     super();
+
     this._resettableState = () =>
       deepCopy({
+        ...baseState,
         ...resettableState,
       });
+
     this.state = {
       headers,
       ...this._resettableState(),
     };
   }
 }
+
