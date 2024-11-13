c4b3568ce75e47ec5514f860099986185585aa9c
Lera24 <bedulinavalera21@gmail.com>
2024-11-13 15:58:00 +0200

    feature: refactor start-page and configuration [WTEL-4710](https://webitel.atlassian.net/browse/WTEL-4710)

--- src/modules/start-page/components/the-start-page.vue
+++ src/modules/start-page/components/the-start-page.vue
@@ -1,51 +1,50 @@
 <template>
-  <div>
-    <start-page :nav="nav" :appLogo="logo"/>
-  </div>
+  <wt-start-page
+    :nav="nav"
+    :app-logo="logo"
+  />
 </template>
 
 <script setup>
-  import { useI18n } from 'vue-i18n';
-  import StartPage from '@webitel/ui-sdk/src/modules/StartPage/components/the-start-page.vue';
-  import CrmSections from '@webitel/ui-sdk/src/enums/WebitelApplications/CrmSections.enum';
-  import StartLogoLight from '../assets/start-page-logo-light.svg';
-  import StartLogoDark from '../assets/start-page-logo-dark.svg';
-  import ConfigurationLight from '../assets/configuration-section-light.svg';
-  import ConfigurationDark from '../assets/configuration-section-dark.svg';
-  import ContactsLight from '../assets/contacts-section-light.svg';
-  import ContactsDark from '../assets/contacts-section-dark.svg';
+import { useI18n } from 'vue-i18n';
+import CrmSections from '@webitel/ui-sdk/src/enums/WebitelApplications/CrmSections.enum';
+import WtStartPage from '@webitel/ui-sdk/src/components/on-demand/wt-start-page/components/wt-start-page.vue';
+import LogoLight from '../assets/crm-logo-light.svg';
+import LogoDark from '../assets/crm-logo-dark.svg';
+import ConfigurationImgLight from '../assets/configuration-section-img-light.svg';
+import ConfigurationImgDark from '../assets/configuration-section-img-dark.svg';
+import ContactsImgLight from '../assets/contacts-section-img-light.svg';
+import ContactsImgDark from '../assets/contacts-section-img-dark.svg';
 
-  const { t } = useI18n();
+const { t } = useI18n();
 
-  const logo = {
-    light: StartLogoLight,
-    dark: StartLogoDark
-  }
+const logo = {
+  light: LogoLight,
+  dark: LogoDark,
+};
 
-  const nav = [
-    {
-      value: CrmSections.CONTACTS,
-      route: '/contacts',
-      name: t(`startPage.${CrmSections.CONTACTS}.name`),
-      text: t(`startPage.${CrmSections.CONTACTS}.text`),
-      images: {
-        light: ContactsLight,
-        dark: ContactsDark,
-      },
-      disabled: false
+const nav = [
+  {
+    value: CrmSections.CONTACTS,
+    route: '/contacts',
+    name: t(`startPage.${CrmSections.CONTACTS}.name`),
+    text: t(`startPage.${CrmSections.CONTACTS}.text`),
+    images: {
+      light: ContactsImgLight,
+      dark: ContactsImgDark,
     },
-    {
-      value: CrmSections.CONFIGURATION,
-      route: '/configuration',
-      name: t(`startPage.${CrmSections.CONFIGURATION}.name`),
-      text: t(`startPage.${CrmSections.CONFIGURATION}.text`),
-      images: {
-        light: ConfigurationLight,
-        dark: ConfigurationDark,
-      },
-      disabled: false
+  },
+  {
+    value: 'configuration',
+    route: '/configuration',
+    name: t(`startPage.configuration.name`),
+    text: t(`startPage.configuration.text`),
+    images: {
+      light: ConfigurationImgLight,
+      dark: ConfigurationImgDark,
     },
-  ]
+  },
+];
 </script>
 
 <style scoped>
