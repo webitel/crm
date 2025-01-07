b85aa0e5a4275c95e21b2024f315a66e1dc2cdb1
Stas Kozak <kozak.stasan@gmail.com>
2025-01-07 10:40:47 +0200

    feat: add todo

--- src/modules/configuration/modules/lookups/modules/service-catalogs/modules/services/store/_internals/headers.js
+++ src/modules/configuration/modules/lookups/modules/service-catalogs/modules/services/store/_internals/headers.js
@@ -1,6 +1,7 @@
 import { SortSymbols } from '@webitel/ui-sdk/src/scripts/sortQueryAdapters';
 
 export default [
+  // TODO Implement another columns after finished created service https://webitel.atlassian.net/browse/WTEL-5658
   {
     value: 'name',
     locale: 'reusable.name',
