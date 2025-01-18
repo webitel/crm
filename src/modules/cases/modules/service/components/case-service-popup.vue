b5cf041c3790808a80c3bf90d2ee709565135142
Stas Kozak <kozak.stasan@gmail.com>
2025-01-18 18:54:37 +0200

    feat: implement base we-tree with base search bar for select service on case service popup [WTEL-5301](https://webitel.atlassian.net/browse/WTEL-5301)

--- src/modules/cases/modules/service/components/case-service-popup.vue
+++ src/modules/cases/modules/service/components/case-service-popup.vue
@@ -1,9 +1,29 @@
 <template>
   <wt-popup
     :shown="shown"
+    @close="close"
   >
+    <template #header>
+      {{ $t('cases.selectAService') }}
+    </template>
+    <template #main>
+      <wt-search-bar
+        :value="search"
+        placeholder="Search"
+        @input="search = $event"
+      />
+      <wt-tree
+        :model-value="selectedElement"
+        style="height: 350px"
+        :data="catalogData"
+        item-label="name"
+        children-prop="service"
+        @update:model-value="selectedElement = $event"
+      />
+    </template>
     <template #actions>
       <wt-button
+        :disabled="!selectedElement"
         @click="save"
       >
         {{ $t('reusable.add') }}
@@ -20,25 +40,53 @@
 
 <script setup>
 import { serviceCatalogData } from '../api/example.js';
+import { onMounted, ref } from 'vue';
+import CatalogsAPI from '../../../../configuration/modules/lookups/modules/service-catalogs/api/service-catalogs.js';
 
 const props = defineProps({
+  value: {
+    type: Object,
+    required: true,
+  },
   shown: {
     type: Boolean,
     required: true,
   },
 });
 
+const check = (event) => {
+  console.log('event', event)
+}
+
+const selectedElement = ref(props.value ?? null)
+const search = ref('')
+
 const emit = defineEmits(['save', 'close']);
 
 
 function save() {
-  emit('save', serviceCatalogData);
+  emit('save', selectedElement.value);
   close();
 }
 
 function close() {
   emit('close');
 }
+
+const catalogData = ref([]);
+
+const loadCatalogs = async () => {
+   const { items } = await CatalogsAPI.getList({
+     size: -1,
+     fields: ['id', 'name', 'service'],
+  });
+
+  catalogData.value = items;
+}
+
+onMounted(() => {
+  loadCatalogs();
+});
 </script>
 
 <style lang="scss" scoped>
