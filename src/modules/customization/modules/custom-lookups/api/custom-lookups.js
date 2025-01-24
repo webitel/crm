c276f0876325cf7f0298f1ddd79a6b679c1d0672
Stas Kozak <kozak.stasan@gmail.com>
2025-01-24 18:34:19 +0200

    feat: implement base page with get info about custom lookup. Update custom lookup api [WTEL-5989](https://webitel.atlassian.net/browse/WTEL-5989)

--- src/modules/customization/modules/custom-lookups/api/custom-lookups.js
+++ src/modules/customization/modules/custom-lookups/api/custom-lookups.js
@@ -3,39 +3,62 @@ import {
   getDefaultGetListResponse,
   getDefaultGetParams,
   getDefaultInstance,
+  getDefaultOpenAPIConfig,
 } from '@webitel/ui-sdk/src/api/defaults/index.js';
 import applyTransform, {
   camelToSnake,
-  generateUrl,
   merge,
   notify,
   sanitize,
   snakeToCamel,
+  starToSearch,
 } from '@webitel/ui-sdk/src/api/transformers/index.js';
+import { DictionariesApiFactory } from 'webitel-sdk';
 
 const instance = getDefaultInstance();
+const configuration = getDefaultOpenAPIConfig();
 
 const baseUrl = '/types/dictionaries';
+
+const dictionariesService = new DictionariesApiFactory(
+  configuration,
+  '',
+  instance,
+);
+
+const fieldsToSend = ['name', 'description', 'dictionary'];
 
 const getCustomLookupsList = async (params) => {
   const fieldsToSend = ['page', 'size', 'q', 'sort', 'fields', 'id'];
 
-  const url = applyTransform(params, [
+  const { page, size, fields, sort, id, q } = applyTransform(params, [
     merge(getDefaultGetParams()),
+    starToSearch('search'),
     (params) => ({ ...params, q: params.search }),
     sanitize(fieldsToSend),
     camelToSnake(),
-    generateUrl(baseUrl),
   ]);
   try {
-    const response = await instance.get(url);
+    const response = await dictionariesService.searchType(
+      q,
+      id,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      size,
+      page,
+      sort,
+      fields,
+    );
     const { data, next } = applyTransform(response.data, [
-      snakeToCamel(),
       merge(getDefaultGetListResponse()),
     ]);
-
     return {
-      items: applyTransform(data, []),
+      items: applyTransform(data, [snakeToCamel()]),
       next,
     };
   } catch (err) {
@@ -43,8 +66,21 @@ const getCustomLookupsList = async (params) => {
   }
 };
 
+const getCustomLookup = async ({ itemId: itemRepo }) => {
+  try {
+    const response = await dictionariesService.locateType(
+      itemRepo,
+      fieldsToSend,
+    );
+    return applyTransform(response.data, [snakeToCamel()]);
+  } catch (err) {
+    throw applyTransform(err, [notify]);
+  }
+};
+
 const CustomLookupsApi = {
   getList: getCustomLookupsList,
+  get: getCustomLookup,
 
   ...generatePermissionsApi(baseUrl),
 };
