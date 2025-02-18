e34d7f463fb470d66b6aee596d076b270e1cc64a
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-02-18 22:29:34 +0200

    fix: cases table rows selection [WTEL-6250](https://webitel.atlassian.net/browse/WTEL-6250)

--- src/modules/cases/store/cases.js
+++ src/modules/cases/store/cases.js
@@ -1,6 +1,4 @@
-import {
-  createObjectPermissionsStoreModule
-} from '@webitel/ui-sdk/src/modules/ObjectPermissions/store/index';
+import { createObjectPermissionsStoreModule } from '@webitel/ui-sdk/src/modules/ObjectPermissions/store/index';
 import {
   createApiStoreModule,
   createBaseStoreModule,
@@ -15,7 +13,7 @@ import comments from '../modules/case-info/modules/comments/store/comments.js';
 import relatedCases from '../modules/case-info/modules/related-cases/store/related-cases.js';
 import filters from '../modules/filters/store/filters.js';
 import service from '../modules/service/store/service.js';
-import headers from './_internals/headers.js';
+import { headers } from './_internals/headers';
 
 const resetCardState = {
   itemInstance: {
