027326d140b3587631b084d600628ca2e2fdacd5
Lera24 <bedulinavalera21@gmail.com>
2025-02-21 18:52:56 +0200

    update:webitel/ui-sdk version[WTEL-5361](https://webitel.atlassian.net/browse/WTEL-5361)

--- src/modules/cases/api/stringifyCaseFilters.js
+++ src/modules/cases/api/stringifyCaseFilters.js
@@ -1,23 +1,48 @@
 const filterTransformersMap = {
   'createdAtFrom': (value) => `created_at.from=${value}`,
   'createdAtTo': (value) => `created_at.to=${value}`,
-  'statusCase': (value) => `status=${value.conditions}`,
+  'statusCase': (value) => `status_condition=${value.conditions}`,
   'sourceCase': (value) => `source=${value}`,
-  'author': (value) => `author=${value}`,
+  'author': (value) => `created_by=${value}`,
   'reporter': (value) => `reporter=${value}`,
   'impacted': (value) => `impacted=${value}`,
   'assignee': (value) => value.unassigned ? `assignee=${value.list},${null}` : `assignee=${value.list}`,
-  'contactGroup': (value) => value.unassigned ? `contact_group=${value.list},${null}` : `contact_group=${value.list}`,
+  'contactGroup': (value) => value.unassigned ? `group=${value.list},${null}` : `group=${value.list}`,
   'priorityCase': (value) => `priority=${value}`,
-  'closeReasonGroupsCase': (value) => `close_reason_group=${value.conditions}`,
-  'rating': (value) => `rating=${value}`,
+  'closeReasonGroupsCase': (value) => `close_reason=${value.conditions}`,
+  'rating': (value) => {
+    const arr = [];
+    if(value.from) arr.push(`rating.from=${value.from}`);
+    if(value.to) arr.push(`rating.to=${value.to}`);
+    return arr;
+  },
   'service': (value) => `service=${value}`,
   'sla': (value) => `sla=${value}`,
   'slaCondition': (value) => `sla_condition=${value.conditions}`,
-  'reactionTime': (value) => `reaction_time=${value}`,
-  'resolutionTime': (value) => `resolution_time=${value}`,
-  'actualReactionTime': (value) => `actual_reaction_time=${value}`,
-  'actualResolutionTime': (value) => `actual_resolution_time=${value}`,
+  'reactionTime': (value) => {
+    const arr = [];
+    if(value.from) arr.push(`planned_reaction_at.from=${value.from}`);
+    if(value.to) arr.push(`planned_reaction_at.to=${value.to}`);
+    return arr;
+  },
+  'resolutionTime': (value) => {
+    const arr = [];
+    if(value.from) arr.push(`planned_resolved_at.from=${value.from}`);
+    if(value.to) arr.push(`planned_resolved_at.to=${value.to}`);
+    return arr;
+  },
+  'actualReactionTime': (value) => {
+    const arr = [];
+    if(value.from) arr.push(`reacted_at.from=${value.from}`);
+    if(value.to) arr.push(`reacted_at.to=${value.to}`);
+    return arr;
+  },
+  'actualResolutionTime': (value) => {
+    const arr = [];
+    if(value.from) arr.push(`resolved_at.from=${value.from}`);
+    if(value.to) arr.push(`resolved_at.to=${value.to}`);
+    return arr;
+  },
   'hasAttachment': (value) => `attachments=${value}`,
 };
 
