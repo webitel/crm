c9b34009843a46d132fb9abcdd5ead2c53ee7a39
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-02-25 17:59:31 +0200

    fix: app localization changing [WTEL-6285](https://webitel.atlassian.net/browse/WTEL-6285)

--- src/app.vue
+++ src/app.vue
@@ -2,6 +2,22 @@
   <router-view />
 </template>
 
+<script lang="ts" setup>
+import {onMounted} from "vue";
+import {useI18n} from "vue-i18n";
+
+const { locale } = useI18n();
+
+function setLanguage() {
+  const lang = localStorage.getItem('lang');
+  if (lang) locale.value = lang;
+}
+
+onMounted(() => {
+  setLanguage();
+});
+</script>
+
 <style lang="scss">
 #app {
   min-width: 100%;
