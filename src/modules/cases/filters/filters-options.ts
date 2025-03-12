45453e79208e8544c4fd8cc02e9177277cb68ee7
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-03-12 23:28:13 +0200

    feat: cases presets [WTEL-3751](https://webitel.atlassian.net/browse/WTEL-3752), [WTEL-3752](https://webitel.atlassian.net/browse/WTEL-3752)
    (cherry picked from commit 0c693729e400d821337d524c7f257e6cbb1d0cd4)

---
+++ src/modules/cases/filters/filters-options.ts
@@ -0,0 +1,24 @@
+import { FilterOption } from "@webitel/ui-sdk/src/modules/Filters/v2/filters/enums/FilterOption";
+
+export const filtersOptions: FilterOption[] = [
+  FilterOption.CreatedAtFrom,
+  FilterOption.CreatedAtTo,
+  FilterOption.Status,
+  FilterOption.Source,
+  FilterOption.Service,
+  FilterOption.Author,
+  FilterOption.Reporter,
+  FilterOption.Impacted,
+  FilterOption.Assignee,
+  FilterOption.ContactGroup,
+  FilterOption.Priority,
+  FilterOption.CloseReasonGroups,
+  FilterOption.Rating,
+  FilterOption.Sla,
+  FilterOption.SlaCondition,
+  FilterOption.ReactionTime,
+  FilterOption.ResolutionTime,
+  FilterOption.ActualReactionTime,
+  FilterOption.ActualResolutionTime,
+  FilterOption.HasAttachment,
+];
