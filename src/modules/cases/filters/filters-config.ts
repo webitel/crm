1c1c58159e0d5b479514a231e97c28f64ab5a870
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-03-12 21:40:43 +0200

    refactor: filter options  [WTEL-3751(https://webitel.atlassian.net/browse/WTEL-3751), WTEL-3752(https://webitel.atlassian.net/browse/WTEL-3752)]
    (cherry picked from commit d2f4fd645a64abad6b8dbf03a62c2d47ba9d4a36)

--- src/modules/cases/filters/filters-config.ts
+++ src/modules/cases/filters/filters-config.ts
@@ -1,133 +1,24 @@
-import {
-  ActualReactionTimeFilter,
-  ActualReactionTimeFilterPreview,
-  ActualResolutionTimeFilter,
-  ActualResolutionTimeFilterPreview,
-  AssigneeFilter,
-  AssigneeFilterPreview,
-  AuthorFilter,
-  AuthorFilterPreview,
-  CasePriorityFilter,
-  CasePriorityFilterPreview,
-  CaseServiceFilter,
-  CaseServiceFilterPreview,
-  CaseSourceFilter,
-  CaseSourceFilterPreview,
-  CaseStatusFilter,
-  CaseStatusFilterPreview,
-  CloseReasonGroupsCaseFilter,
-  CloseReasonGroupsCaseFilterPreview,
-  ContactGroupFilter,
-  ContactGroupFilterPreview,
-  CreatedAtFromFilter,
-  CreatedAtFromFilterPreview,
-  CreatedAtToFilter,
-  CreatedAtToFilterPreview,
-  HasAttachmentFilter,
-  HasAttachmentFilterPreview,
-  ImpactedFilter,
-  ImpactedFilterPreview,
-  RatingFromToFilter,
-  RatingFromToFilterPreview,
-  ReactionTimeFilter,
-  ReactionTimeFilterPreview,
-  ReporterFilter,
-  ReporterFilterPreview,
-  ResolutionTimeFilter,
-  ResolutionTimeFilterPreview,
-  SlaConditionFilter,
-  SlaConditionFilterPreview,
-  SlaFilter,
-  SlaFilterPreview,
-} from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/index.js';
-import { Component } from 'vue';
+import {FilterName} from "@webitel/ui-sdk/src/modules/Filters/v2/filters/index";
 
-interface FiltersConfig {
-  [key: string]: {
-    valueField: Component
-    previewField: Component
-  }
-}
-
-export const filtersConfig: FiltersConfig = {
-  createdAtFrom: {
-    valueField: CreatedAtFromFilter,
-    previewField: CreatedAtFromFilterPreview,
-  },
-  createdAtTo: {
-    valueField: CreatedAtToFilter,
-    previewField: CreatedAtToFilterPreview,
-  },
-  status: {
-    valueField: CaseStatusFilter,
-    previewField: CaseStatusFilterPreview,
-  },
-  source: {
-    valueField: CaseSourceFilter,
-    previewField: CaseSourceFilterPreview,
-  },
-  service: {
-    valueField: CaseServiceFilter,
-    previewField: CaseServiceFilterPreview,
-  },
-  author: {
-    valueField: AuthorFilter,
-    previewField: AuthorFilterPreview,
-  },
-  reporter: {
-    valueField: ReporterFilter,
-    previewField: ReporterFilterPreview,
-  },
-  impacted: {
-    valueField: ImpactedFilter,
-    previewField: ImpactedFilterPreview,
-  },
-  assignee: {
-    valueField: AssigneeFilter,
-    previewField: AssigneeFilterPreview,
-  },
-  contactGroup: {
-    valueField: ContactGroupFilter,
-    previewField: ContactGroupFilterPreview,
-  },
-  priority: {
-    valueField: CasePriorityFilter,
-    previewField: CasePriorityFilterPreview,
-  },
-  closeReasonGroups: {
-    valueField: CloseReasonGroupsCaseFilter,
-    previewField: CloseReasonGroupsCaseFilterPreview,
-  },
-  rating: {
-    valueField: RatingFromToFilter,
-    previewField: RatingFromToFilterPreview,
-  },
-  sla: {
-    valueField: SlaFilter,
-    previewField: SlaFilterPreview,
-  },
-  slaCondition: {
-    valueField: SlaConditionFilter,
-    previewField: SlaConditionFilterPreview,
-  },
-  reactionTime: {
-    valueField: ReactionTimeFilter,
-    previewField: ReactionTimeFilterPreview,
-  },
-  resolutionTime: {
-    valueField: ResolutionTimeFilter,
-    previewField: ResolutionTimeFilterPreview,
-  },
-  actualReactionTime: {
-    valueField: ActualReactionTimeFilter,
-    previewField: ActualReactionTimeFilterPreview,
-  },
-  actualResolutionTime: {
-    valueField: ActualResolutionTimeFilter,
-    previewField: ActualResolutionTimeFilterPreview,
-  },
-  hasAttachment: {
-    valueField: HasAttachmentFilter,
-    previewField: HasAttachmentFilterPreview,
-  },
-};
+export const filtersConfig: FilterName[] = [
+  'createdAtFrom',
+  'createdAtTo',
+  'status',
+  'source',
+  'service',
+  'author',
+  'reporter',
+  'impacted',
+  'assignee',
+  'contactGroup',
+  'priority',
+  'closeReasonGroups',
+  'rating',
+  'sla',
+  'slaCondition',
+  'reactionTime',
+  'resolutionTime',
+  'actualReactionTime',
+  'actualResolutionTime',
+  'hasAttachment',
+];
