3a9eddb9b7d2d7ffae380bdc575f70512ecd3d13
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-07-23 18:40:21 +0300

    refactor: card store and validations related cleanups [WTEL-7046](https://webitel.atlassian.net/browse/WTEL-7046)

--- src/modules/configuration/modules/lookups/modules/sources/stores/card/caseSourcesCardStore.ts
+++ src/modules/configuration/modules/lookups/modules/sources/stores/card/caseSourcesCardStore.ts
@@ -1,11 +1,11 @@
 import { CaseSourcesAPI } from '@webitel/api-services/api';
 import {createSourceBody as standardValidationSchema} from "@webitel/api-services/gen";
-// import { WebitelCasesSource } from "@webitel/api-services/gen";
+import { WebitelCasesSource } from "@webitel/api-services/gen";
 import {createCardStore} from "@webitel/ui-datalist/card";
 
 import { CaseSourcesNamespace } from "../../namespace";
 
-export const useCaseSourcesCardStore = createCardStore/*<WebitelCasesSource>*/({
+export const useCaseSourcesCardStore = createCardStore<WebitelCasesSource>({
   namespace: `${CaseSourcesNamespace}/card`,
   apiModule: CaseSourcesAPI,
   standardValidationSchema,
