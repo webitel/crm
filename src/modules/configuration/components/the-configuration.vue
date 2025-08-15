a44294378e974ee9dd4c4eb21bbe530b5d4142f0
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-08-15 14:45:29 +0300

    feat: custom lookups inside the-configuration nav access filtering [WTEL-7082](https://webitel.atlassian.net/browse/WTEL-7082)

--- src/modules/configuration/components/the-configuration.vue
+++ src/modules/configuration/components/the-configuration.vue
@@ -10,6 +10,7 @@ import CrmSections from '@webitel/ui-sdk/src/enums/WebitelApplications/CrmSectio
 import { computed, reactive, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
+import { WtNavigationMenu } from '@webitel/ui-sdk/components';
 
 import customizationIcon from '../../../app/assets/icons/sprite/crm-customization.svg';
 import lookupsIcon from '../../../app/assets/icons/sprite/crm-lookups.svg';
@@ -32,84 +33,82 @@ const navAccessReducer = (reducedNav, currentNav) => {
   return routeAccessGuard(route) === true ? [...reducedNav, currentNav] : reducedNav;
 };
 
-const customLookupsNav = computed(() => {
-  return customLookups.value.map((item) => ({
-      value: item.id,
+const nav = computed(() => {
+  const customLookupsNav = customLookups.value.map((item) => ({
+      value: item.repo,
       name: item.name,
       route: `lookups/${item.repo}`,
     }));
-});
 
-const nav = computed(() => [
+  return [
   {
     value: 'lookups',
-    name: computed(() => t('lookups.lookups')),
+    name: t('lookups.lookups'),
     subNav: [
       {
         value: CrmSections.CONTACT_GROUPS,
-        name: computed(() => t('lookups.contactGroups.contactGroups', 2)),
+        name: t('lookups.contactGroups.contactGroups', 2),
         route: 'lookups/contact-groups',
       },
       {
         value: CrmSections.PRIORITIES,
-        name: computed(() => t('vocabulary.priority', 2)),
+        name: t('vocabulary.priority', 2),
         route: 'lookups/priorities',
       },
       {
         value: CrmSections.CLOSE_REASON_GROUPS,
-        name: computed(() =>
-          t('lookups.closeReasonGroups.closeReasonGroups', 2),
-        ),
+        name: t('lookups.closeReasonGroups.closeReasonGroups', 2),
         route: 'lookups/close-reason-groups',
       },
       {
         value: CrmSections.STATUSES,
-        name: computed(() => t(`lookups.statuses.statuses`, 2)),
+        name: t(`lookups.statuses.statuses`, 2),
         route: 'lookups/statuses',
       },
       {
         value: CrmSections.SOURCES,
-        name: computed(() => t('lookups.sources.sources', 2)),
+        name: t('lookups.sources.sources', 2),
         route: 'lookups/sources',
       },
       {
         value: CrmSections.SLAS,
-        name: computed(() => t('lookups.slas.slas', 2)),
+        name: t('lookups.slas.slas', 2),
         route: 'lookups/slas',
       },
       {
         value: CrmSections.SERVICE_CATALOGS,
-        name: computed(() => t('lookups.serviceCatalogs.serviceCatalogs', 2)),
+        name: t('lookups.serviceCatalogs.serviceCatalogs', 2),
         route: 'lookups/service-catalogs',
       },
-      ...customLookupsNav.value,
+      ...customLookupsNav,
     ],
   },
   {
     value: 'customization',
-    name: computed(() => t('customization.customization')),
+    name: t('customization.customization'),
     subNav: [
       {
         value: 'type-extension-cases',
-        name: computed(() => t('customization.extensions.cases')),
+        name: t('customization.extensions.cases'),
         route: 'customization/types-extensions/cases',
       },
       {
         value: 'type-extension-contact',
-        name: computed(() => t('customization.extensions.contacts')),
+        name: t('customization.extensions.contacts'),
         route: 'customization/types-extensions/contacts',
       },
       {
         value: CrmSections.CUSTOM_LOOKUPS,
-        name: computed(() => t('customization.customLookups.customLookups')),
+        name: t('customization.customLookups.customLookups'),
         route: 'customization/custom-lookups',
       },
     ],
-  },
-]);
+    },
+  ];
+});
 
 const accessibleNav = computed(() => {
-  return nav.reduce(navAccessReducer, []);
+  return nav.value.reduce(navAccessReducer, []);
 });
 
 const loadCustomLookups = async () => {
