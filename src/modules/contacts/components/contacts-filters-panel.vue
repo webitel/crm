40081d66ca69709c60b551cf147e50142ceca53d
Олександр Пальонний <s2567k@gmail.com>
2026-04-09 14:26:42 +0300

    fix: added custom filters for contacts table [WTEL-6932](https://webitel.atlassian.net/browse/WTEL-6932)

--- src/modules/contacts/components/contacts-filters-panel.vue
+++ src/modules/contacts/components/contacts-filters-panel.vue
@@ -1,7 +1,9 @@
 <template>
   <table-filters-panel
+    v-if="!isLoadingExtensionFields"
     :filters-manager="filtersManager"
     :filter-options="filtersOptions"
+    :filterable-extension-fields="extensionFields"
     @filter:add="addFilter"
     @filter:update="updateFilter"
     @filter:delete="deleteFilter"
@@ -14,6 +16,7 @@
 import { TableFiltersPanelComponent as TableFiltersPanel } from '@webitel/ui-datalist/filters';
 import { storeToRefs } from 'pinia';
 
+import { useExtensionFields } from '../../customization/modules/wt-type-extension/composable/useExtensionFields';
 import { filtersOptions } from '../configs/filtersOptions';
 import { useContactsStore } from '../stores/contacts';
 
@@ -25,6 +28,16 @@ const tableStore = useContactsStore();
 const { filtersManager } = storeToRefs(tableStore);
 
 const { addFilter, updateFilter, deleteFilter } = tableStore;
+
+const {
+	fields: extensionFields,
+	isLoading: isLoadingExtensionFields,
+	getFields,
+} = useExtensionFields({
+	type: 'contacts',
+});
+
+getFields();
 
 const resetFilters = () => {
 	filtersManager.value.reset({
