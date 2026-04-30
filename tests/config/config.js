8f046fdfe2d9d1f520a8fd51b27a27056541ca5a
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-04-30 18:44:47 +0300

    tests: fixed existing tests and tests configs, remove (most of all) broken [WTEL-9502](https://webitel.atlassian.net/browse/WTEL-9502)

--- tests/config/config.js
+++ tests/config/config.js
@@ -1,5 +1,6 @@
 import { config } from '@vue/test-utils';
 import axiosMock from '@webitel/ui-sdk/src/tests/mocks/axiosMock'; // import instance from '../../src/app/api/instance.js';
+import { createPinia } from 'pinia';
 
 import i18n from '../../src/app/locale/i18n';
 import WebitelUi from '../../src/app/plugins/webitel/ui-sdk';
@@ -7,6 +8,7 @@ import WebitelUi from '../../src/app/plugins/webitel/ui-sdk';
 config.global.plugins = [
 	WebitelUi,
 	i18n,
+	createPinia(),
 ];
 
 window.scrollTo = () => {};
@@ -18,6 +20,13 @@ vi.doMock('@webitel/ui-sdk/src/api/axios/generateInstance.js', () => ({
 
 beforeAll(async () => {
 	const store = (await import('../../src/app/store/index.js')).default;
-	const router = (await import('../../src/app/router/index.js')).default;
+	const router = (await import('../../src/app/router')).default;
+	const { createUserAccessControl } = await import(
+		'../../src/app/composables/useUserAccessControl'
+	);
+	const { useUserinfoStore } = await import(
+		'../../src/modules/userinfo/store/userinfoStore'
+	);
+	createUserAccessControl(useUserinfoStore);
 	store.commit('SET_ROUTER', router);
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,44 @@
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    alias: {
-      vue: 'vue',
-    },
-    server: {
-      deps: {
-        inline: ['@webitel/ui-sdk'],
-      },
-    },
-    coverage: {
-      enabled: false,
-      reporter: 'json',
-    },
-    environment: 'happy-dom',
-    setupFiles: ['./tests/config/config.js'],
-  },
+	plugins: [
+		vue({
+			template: {
+				compilerOptions: {
+					compatConfig: {
+						MODE: 2,
+					},
+					isCustomElement: (tag) => tag.startsWith('media-'),
+				},
+			},
+		}),
+	],
+	test: {
+		globals: true,
+		alias: {
+			vue: 'vue',
+			'@aliasedDeps/api-services/axios': resolve(
+				__dirname,
+				'src/app/api/instance',
+			),
+		},
+		server: {
+			deps: {
+				inline: [
+					'@webitel/ui-sdk',
+					'@webitel/ui-datalist',
+				],
+			},
+		},
+		coverage: {
+			enabled: false,
+			reporter: 'json',
+		},
+		environment: 'happy-dom',
+		setupFiles: [
+			'./tests/config/config.js',
+		],
+	},
 });
